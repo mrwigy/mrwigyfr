486a87dc95f648059af78c14a071d00ea3afdba3
JasonFelgenhauer <jasonfel1@outlook.be>
2024-04-10 23:20:37 +0200

    Add darkmode & lightmode

--- src/views/components/NavContent.tsx
+++ src/views/components/NavContent.tsx
@@ -2,9 +2,11 @@ import { Component } from 'react';
 import { List, ListItem, Link, IconButton, Stack, Typography } from '@mui/material';
 
 import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import BedtimeIcon from '@mui/icons-material/Bedtime';
 
 type NavContentState = {
 	inDrawer: boolean;
+	darkMode: boolean;
 };
 
 export default class NavContent extends Component<any, NavContentState> {
@@ -13,11 +15,32 @@ export default class NavContent extends Component<any, NavContentState> {
 
 		this.state = {
 			inDrawer: this.props.inDrawer,
+			darkMode: false,
 		};
 	}
 
+	handleDarkModeToggle = () => {
+		const { darkMode } = this.state;
+
+		if (darkMode) {
+			this.setState({ darkMode: false });
+			document.documentElement.style.setProperty('--rich-black', '#030712');
+			document.documentElement.style.setProperty('--light-rich-black', '#111827');
+			document.documentElement.style.setProperty('--gunmetal', '#1f2937');
+			document.documentElement.style.setProperty('--charcoal', '#374151');
+			document.documentElement.style.setProperty('--lavender', '#e5e7fa');
+		} else {
+			this.setState({ darkMode: true });
+			document.documentElement.style.setProperty('--rich-black', '#FFFFFF');
+			document.documentElement.style.setProperty('--light-rich-black', '#F9FAFB');
+			document.documentElement.style.setProperty('--gunmetal', '#FFFFFF');
+			document.documentElement.style.setProperty('--charcoal', '#E5E7EB');
+			document.documentElement.style.setProperty('--lavender', '#111827');
+		}
+	};
+
 	render() {
-		const { inDrawer } = this.state;
+		const { inDrawer, darkMode } = this.state;
 
 		return (
 			<>
@@ -48,13 +71,13 @@ export default class NavContent extends Component<any, NavContentState> {
 					{inDrawer ? (
 						<Stack className='nav_container_drawer_bottom'>
 							<Typography className='nav_container_drawer_bottom_typo'>Toggle Light/Dark Mode</Typography>
-							<IconButton className='nav_container_navigation_action_button' aria-label='Toggle light/dark mode'>
-								<WbSunnyIcon className='nav_container_navigation_action_button_icon' />
+							<IconButton className='nav_container_navigation_action_button' aria-label='Toggle light/dark mode' onClick={this.handleDarkModeToggle}>
+								{darkMode ? <BedtimeIcon className='nav_container_navigation_action_button_icon' /> : <WbSunnyIcon className='nav_container_navigation_action_button_icon' />}
 							</IconButton>
 						</Stack>
 					) : (
-						<IconButton className='nav_container_navigation_action_button' aria-label='Toggle light/dark mode'>
-							<WbSunnyIcon className='nav_container_navigation_action_button_icon' />
+						<IconButton className='nav_container_navigation_action_button' aria-label='Toggle light/dark mode' onClick={this.handleDarkModeToggle}>
+							{darkMode ? <BedtimeIcon className='nav_container_navigation_action_button_icon' /> : <WbSunnyIcon className='nav_container_navigation_action_button_icon' />}
 						</IconButton>
 					)}
 				</Stack>
